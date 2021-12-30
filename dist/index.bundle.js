3a9107cedc017c4788e278a6eae8bb7048c535ad
funkosaur <mario.bojkovski@outlook.com>
2021-12-30 23:58:22 +0000

    beginning

---
+++ dist/index.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("ayyy")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsbUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zaW5nYXBvcmVraXRjaGVuLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiYXl5eVwiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
